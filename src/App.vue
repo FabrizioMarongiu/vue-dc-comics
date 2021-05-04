1cdebd02796264e6a9908dd1a503a1f7cf5d0827
FabrizioMarongiu <78483709+FabrizioMarongiu@users.noreply.github.com>
2021-05-04 18:56:23 +0200

    Create array

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- HEADER -->
+    <Header/>
+
+    <!-- MAIN -->
+    <main>main</main>
+
+    <!-- FOOTER -->
+    <footer>footer</footer>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height:100vh;
+}
+.container{
+  width: 1000px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,80 @@
+<template>
+  <header class="header">
+      <div class="container">
+          <img class="logo" src="../assets/dc-comics-assets/img/dc-logo.png" alt="Dc Logo">
+
+
+
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data(){
+        return {
+             menu: [
+                 {
+                     dir: 'characters',
+                     visible: false,
+                 },
+                 {
+                     dir: 'comics',
+                     visible: false,
+                 },
+                 {
+                     dir: 'movies',
+                     visible: false,
+                 },
+                 {
+                     dir: 'tv',
+                     visible: false,
+                 },
+                 {
+                     dir: 'games',
+                     visible: false,
+                 },
+                 {
+                     dir: 'collectibles',
+                     visible: false,
+                 },
+                 {
+                     dir: 'video',
+                     visible: false,
+                 },
+                 {
+                     dir: 'fans',
+                     visible: false,
+                 },
+                 {
+                     dir: 'news',
+                     visible: false,
+                 },
+                 {
+                     dir: 'shop',
+                     visible: false,
+                 },
+             ]
+        }
+    }
+}
+</script>
+
+<style>
+    .header{
+        width: 100%;
+        height:100px;
+        background-color:aquamarine;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .logo{
+        width:60px
+    }
+
+
+
+
+</style>
